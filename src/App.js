b9535e9ef85a2e74c03ed37b0f7b56180c3e9ec7
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-02-23 18:17:58 -0300

    Requisito 3 feito

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
     return (
-      <Form />
+      <>
+        <Form />
+        <Card />
+      </>
     );
   }
 }
